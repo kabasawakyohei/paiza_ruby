ac28cf3ef309b6c14087edbed870b3d4ed0da266
kyohei kabasawa <slifekb2@gmail.com>
2024-09-24 11:39:30 +0900

    0924

---
+++ atcoder0924.rb
@@ -0,0 +1,35 @@
+# # N枚 4
+# # 与えられるカード 20 18 2 18
+
+# N = 4
+# arry = [20,18,2,18]
+
+# # 登場人物
+# # Alice
+# # Bob
+
+# # カードの差分は？
+
+# A_total_num = arry
+
+
+# 配列Aの要素を受け取る
+N = gets.to_i
+A = gets.split.map(&:to_i)
+
+# 配列を降順にソートする
+A.sort!.reverse!
+
+# プレイヤー2人の得点を記録する配列
+v = [0, 0]
+
+# 各要素をプレイヤーに交互に割り当てる
+N.times do |i|
+  puts i
+  puts i % 2
+  # v[i % 2] += A[i]
+end
+
+# 答えを計算して出力
+# ans = v[0] - v[1]
+# puts ans
